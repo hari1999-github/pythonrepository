497ce00c48a752a8911cb89fe13c6259b396c6fb
hari1999-github <83200512+hari1999-github@users.noreply.github.com>
2021-04-27 16:54:40 +0530

    Add files via upload

---
+++ Pythonfile1.py
@@ -0,0 +1,3 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("This is my code in python")
