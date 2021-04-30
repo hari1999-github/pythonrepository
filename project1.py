eb3e8a6c2bb944afb2dff1db52bc356e9f898b11
hari1999-github <83200512+hari1999-github@users.noreply.github.com>
2021-04-30 15:32:36 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,3 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("1. Add record \n2. Display all record \n3. search by roll number \n4.Search by name \n5.Modify roll number \n6.Delete by roll number \n7.Exit")
